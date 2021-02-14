d6c37423da878e9ca220c77fc8cf7cbade6b47bd
George Vrettos <lukedevgr@gmail.com>
2021-02-14 23:37:09 +0200

    Add API Gateway

---
+++ api-gateway/src/test/java/gr/mastro/ecommerce/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.mastro.ecommerce.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
